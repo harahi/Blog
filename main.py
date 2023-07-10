014ab4f3ccc519875c261985b3049590fee23ecd
harahi <zjc6650681@gmail.com>
2023-07-10 15:05:30 +0800

    Remove feed

--- main.py
+++ main.py
@@ -270,7 +270,7 @@ def main(token, repo_name, issue_number=None, dir_name=BACKUP_DIR):
     for func in [add_md_firends, add_md_top, add_md_recent, add_md_label, add_md_todo]:
         func(repo, "README.md", me)
 
-    generate_rss_feed(repo, "feed.xml", me)
+    # generate_rss_feed(repo, "feed.xml", me)
     to_generate_issues = get_to_generate_issues(repo, dir_name, issue_number)
 
     # save md files to backup folder
