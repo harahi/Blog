7448a8e9962994c7d9675e71dacaac066ce1c8b4
Harahi <zjc6650681@gmail.com>
2023-10-16 15:12:29 +0800

    sync with https://github.com/yihong0618/gitblog

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ import os
 import re
 
 import markdown
-from marko.ext.gfm import gfm as marko
-from github import Github
 from feedgen.feed import FeedGenerator
+from github import Github
 from lxml.etree import CDATA
+from marko.ext.gfm import gfm as marko
 
-MD_HEAD = """## Blog
+MD_HEAD = """## Gitblog
 My personal blog using issues and GitHub Actions (随意转载，无需署名)
+[RSS Feed](https://raw.githubusercontent.com/{repo_name}/master/feed.xml)
 """
-# [RSS Feed](https://raw.githubusercontent.com/{repo_name}/master/feed.xml)
 
 BACKUP_DIR = "BACKUP"
 ANCHOR_NUMBER = 5
@@ -153,6 +153,9 @@ def add_md_top(repo, md, me):
 def add_md_firends(repo, md, me):
     s = FRIENDS_TABLE_HEAD
     friends_issues = list(repo.get_issues(labels=FRIENDS_LABELS))
+    if not FRIENDS_LABELS or not friends_issues:
+        return
+    friends_issue_number = friends_issues[0].number
     for issue in friends_issues:
         for comment in issue.get_comments():
             if is_hearted_by_me(comment, me):
@@ -163,7 +166,9 @@ def add_md_firends(repo, md, me):
                     pass
     s = markdown.markdown(s, output_format="html", extensions=["extra"])
     with open(md, "a+", encoding="utf-8") as md:
-        md.write("## 友情链接\n")
+        md.write(
+            f"## [友情链接](https://github.com/{str(me)}/gitblog/issues/{friends_issue_number})\n"
+        )
         md.write(s)
         md.write("\n\n")
 
@@ -180,8 +185,8 @@ def add_md_recent(repo, md, me, limit=5):
                     count += 1
                     if count >= limit:
                         break
-        except:
-            return
+        except Exception as e:
+            print(str(e))
 
 
 def add_md_header(md, repo_name):
